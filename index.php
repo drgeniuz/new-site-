517290c01577d353c4ee9713b004a07456d286cc
drgeniuz <goldngeniuz@gmail.com>
2021-12-22 19:40:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php require_once 'includes/Header.php'; ?>
+
+<?php 
+	$title = 'Index';
+	require_once 'includes/Header.php'; 
+	?>
 
     <h1>Hello, world!</h1>
 
-  <?php require_once 'includes/Footer.php'; ?>  
+  <?php require_once 'includes/Footerer.php'; ?>  
